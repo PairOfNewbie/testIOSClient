fa231ce7e09598aa7fdf831523f5640bc0888b54
DaiFengyi <daifengji@163.com>
2016-04-04 21:19:59 +0800

    Support flexible way to test

--- testIOSClient/ViewController.m
+++ testIOSClient/ViewController.m
@@ -4,11 +4,9 @@
 //
 //  Created by DaiFengyi on 16/3/27.
 //  Copyright © 2016年 davidear. All rights
-
-//#define kWYNetworkBaseURLStr @"http://112.74.106.192:2345"
-#define kStyleGet @"http://webapp.styleai.cn/"
 #import "ViewController.h"
 #import "WYNetwork.h"
+#import "WYtool.h"
 @interface ViewController ()<UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *urlField;
 @property (weak, nonatomic) IBOutlet UITextView *returnTextView;
@@ -26,21 +24,38 @@
 }
 
 - (IBAction)StartWebRequest:(UIButton *)sender {
-    [[WYNetwork sharedWYNetwork] HttpGet:@"" parameter:nil success:^(id responseObject) {
-        NSLog(@"success");
+    if (self.urlField.text.length == 0) {
+        [WYTool showMsg:@"url can not be empty"];
+        return;
+    }
+    [WYTool showLoading];
+    [[WYNetwork sharedWYNetwork] HttpGet:self.urlField.text parameter:nil success:^(id responseObject) {
+        [WYTool showMsg:@"success"];
+        self.returnTextView.text = [self stringFormDictionary:responseObject];
     } failure:^(NSError *error) {
-        NSLog(@"failure is %@", error.description);
+        [WYTool showMsg:@"failure"];
+        self.returnTextView.text = error.description;
     }];
     
     
 }
 
 - (IBAction)startCorrectRequest:(UIButton *)sender {
+    [WYTool showLoading];
     [[WYNetwork sharedWYNetwork] HttpGet:@"http://c.m.163.com/nc/article/list/T1348647853363/0-20.html" parameter:nil success:^(id responseObject) {
-        NSLog(@"success");
+        [WYTool showMsg:@"success"];
+        self.returnTextView.text = [self stringFormDictionary:responseObject];
     } failure:^(NSError *error) {
-        NSLog(@"failure is %@", error.description);
+        [WYTool showMsg:@"failure"];
+        self.returnTextView.text = error.description;
     }];
 }
 #pragma mark - UITextViewDelegate
+
+#pragma mark - helper
+- (NSString *)stringFormDictionary:(NSDictionary *)dic {
+    NSData *tempData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:nil];//Json数据
+    NSString *jsonStr = [[NSString alloc] initWithData:tempData encoding:NSUTF8StringEncoding];  //Json数据的可视化字符串
+    return jsonStr;
+}
 @end
